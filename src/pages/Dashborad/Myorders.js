448999731c06d9bc4463742055bad34718ff2122
Ekram Hosen <ekramhosen329@gmail.com>
2022-05-25 22:36:53 +0600

    token for my orders

--- src/pages/Dashborad/Myorders.js
+++ src/pages/Dashborad/Myorders.js
@@ -1,15 +1,30 @@
+import { signOut } from 'firebase/auth';
 import React, {useEffect, useState} from 'react';
 import { useAuthState } from "react-firebase-hooks/auth";
+import { useNavigate } from 'react-router-dom';
 import auth from "../../firebase.init";
 
 const Myorders = () => {
   const [orders, setOrders] = useState([]);
   const [user] = useAuthState(auth)
+  const navigate = useNavigate()
 
   useEffect(()=>{
     if(user){
-      fetch(`https://boiling-garden-19713.herokuapp.com/purchased?userEmail=${user.email}`)
-      .then(res => res.json())
+      fetch(`http://localhost:5000/purchased?userEmail=${user.email}`, {
+        method: "GET",
+        headers: {
+          "authorization": `Bearer ${localStorage.getItem('accessToken')}`
+        }
+      })
+      .then(res =>{
+        console.log('res', res);
+        if(res.status === 401 || res.status === 403){
+            signOut(auth);
+            localStorage.removeItem('accessToken');
+            navigate("/");
+        }
+        return res.json()})
       .then(data => setOrders(data));
     }
   },[user])
